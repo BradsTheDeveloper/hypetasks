4edc16324679ad3cd23a00e7f3575e9914b6c354
BradsTheDeveloper <bradleyaustin2611@gmail.com>
2023-07-12 14:14:45 +0100

    Start sidebar design, alignment issues

---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,129 @@
+<script>
+	let plusButtonClicked = false
+
+	function plusButtonClick() {
+		if (plusButtonClicked) return // prevent toggle when already toggled
+		plusButtonClicked = true
+		setTimeout(() => {
+			plusButtonClicked = false
+		}, 200)
+	}
+</script>
+
+<nav id="sidebar">
+    <div id="sidebarNav">
+        <a id="home" href="/">
+            <i class="fa-solid fa-house"></i>
+            <p>Home</p>
+        </a>
+        <a id="events" href="/events">
+            <i class="fa-solid fa-calendar-day"></i>
+            <p>Events</p>
+        </a>
+        <a id="focus" href="/focus">
+            <i class="fa-solid fa-expand"></i>
+            <p>Focus</p>
+        </a>
+        <a id="archive" href="/archive">
+            <i class="fa-solid fa-folder"></i>
+            <p>Archive</p>
+        </a>
+    </div>
+</nav>
+
+<style>
+
+    :root {
+        --transition-speed: 600ms;
+    }
+
+    #sidebar {
+        position: fixed;
+        background-color: white;
+        box-shadow: 0px 0px 8px #888888;
+        transition: width 600ms ease;
+        overflow: scroll;
+    }
+
+    #sidebarNav {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 100%;
+        width: 100%;
+    }
+
+    #sidebarNav a {
+        display: flex;
+        align-items: center;
+        height: 5rem;
+        color: gray;
+        text-decoration: none;
+        filter: grayscale(100%) opacity(0.7);
+        transition: var(--transition-speed);
+    }
+
+    #sidebarNav a {
+        width: 100%;
+    }
+
+    #sidebarNav:last-child {
+        margin-top: auto;
+    }
+
+    
+    #sidebarNav a p {
+        display: none;
+        margin-left: 1rem;
+        filter: opacity(0);
+        transition: var(--transition-speed);
+    }
+
+    #sidebarNav a i {
+        width: 2rem;
+        min-width: 2rem;
+        margin: 0 1.5rem;
+    }
+
+    
+    /* Large screens */
+    @media only screen and (min-width: 600px) {
+        #sidebar {
+            top: 0;
+            width: 5rem;
+            height: 100vh;
+        }
+
+        #sidebar:hover {
+            width: 16rem;
+        }
+
+        #sidebar:hover a p {
+            display: inline;
+            filter: opacity(1);
+            transform: translateX(0);
+        }
+        
+
+    }
+
+    /* Small screens */
+    @media only screen and (max-width: 600px) {
+        #sidebar {
+            bottom: 0;
+            width: 100vw;
+            height: 3rem;
+            box-shadow: 0px 0px 8px #888888;
+        }
+
+
+        #sidebarNav {
+            flex-direction: row;
+        }
+
+        #sidebarNav a {
+            justify-content: center;
+        }
+    }
+
+</style>
---
+++ src/routes/oldDashHeaderPage.svelte
@@ -0,0 +1,135 @@
+<script>
+
+	import Taskbar  from "../components/Taskbar.svelte";
+	import { quotes } from '/src/stores.js'; 
+	let dashHeaderBG;
+	let dashHeaderText;
+
+	let timeOfDay;
+	const d = new Date();
+	let hour = d.getHours();
+	if (hour < 5) {
+		timeOfDay = "night";
+		dashHeaderBG = '#070808';
+		dashHeaderText = '#ffffff';
+	} else if (hour < 12) {
+		timeOfDay = "morning";
+		dashHeaderBG = '#edeecb';
+	} else if (hour < 17) {
+		timeOfDay = "afternoon";
+		dashHeaderBG = '#edeecb';
+	} else if (hour < 21) {
+		timeOfDay = "evening";
+		dashHeaderBG = '#2c4349';
+		dashHeaderText = '#ffffff';
+	} else if (hour <= 23) {
+		timeOfDay = "night";
+		dashHeaderBG = '#070808';
+		dashHeaderText = '#ffffff';
+	}
+	const randomQuoteID = Math.floor(Math.random() * quotes.length)
+	const randomQuote = quotes[randomQuoteID][0];
+	const randomQuoteAuthor = quotes[randomQuoteID][1];
+
+	let testTasks = [
+		["Do homework", "8/7/23, 17:00", "notDone"],
+		["Do homework", "8/7/23, 17:00", "notDone"],
+		["Do homework", "8/7/23, 17:00", "notDone"],
+	]
+
+</script>
+
+<Taskbar />
+
+<div id="dashHeader" style="--dashHeaderBG: {dashHeaderBG}; --dashHeaderText: {dashHeaderText}; ">
+    <div id="greeting">
+		<div id="mainGreeting">
+			<h2>Good {timeOfDay}, Bradley</h2>
+			<p>Let's get some work done!</p>
+		</div>
+		<div id="greetingQuoteContent">
+            <p>{randomQuote}</p>
+        </div>
+        <p id="greetingQuoteAuthor">{randomQuoteAuthor}</p>
+    </div>
+    <div id="dashHeaderBlocks">
+        {#each testTasks as task, i}
+			<div class="taskBlock"></div>
+		{/each}
+    </div>
+</div>
+
+<style>
+
+	@media (max-width: 768px) {
+		#dashHeader {
+			flex-direction: column;
+		}
+
+		#dashHeaderBlocks {
+			margin: 0;
+			display: flex;
+			flex-direction: row;
+			justify-content: center;
+		}
+	}
+
+	* {
+		box-sizing: border-box;
+	}
+
+	#dashHeader {
+		display: flex;
+		flex-direction: row;
+        box-sizing: border-box;
+		justify-content: space-between;
+		background-color: var(--dashHeaderBG);
+		color: var(--dashHeaderText);
+        margin: 0;
+		padding: 10px 10px;
+		padding-right: 0;
+        width: 100vw;
+        height: max-content;
+	}
+
+	#mainGreeting {
+		
+	}
+
+	#greetingQuoteContent {
+		box-sizing: border-box;
+		width: 100%;
+		height: max-content;
+		background-color: white;
+		color: black;
+		border-radius: 5px;
+		padding: 2px 10px;
+	}
+
+	#greetingQuoteAuthor {
+		margin-top: 10px;
+	}
+
+	#dashHeaderBlocks {
+		height: inherit;
+		margin: 0;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: flex-start;
+	}
+
+	.taskBlock {
+		float: right;
+		background-color: gray;
+		height: 10px;
+		width: 10px;
+		margin: 10px 0 !important ;
+		align-self: flex-end;
+	}
+
+	.taskBlock:first-of-type {
+		margin-top: 0;
+	}
+
+</style> 
