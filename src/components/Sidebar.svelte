7c7e50db992cb30c74474ed5b5ef8baebb426875
BradsTheDeveloper <bradleyaustin2611@gmail.com>
2023-07-12 14:32:18 +0100

    Sidebar center alignment issues fixed

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -11,24 +11,18 @@
 </script>
 
 <nav id="sidebar">
-    <div id="sidebarNav">
         <a id="home" href="/">
             <i class="fa-solid fa-house"></i>
-            <p>Home</p>
         </a>
         <a id="events" href="/events">
             <i class="fa-solid fa-calendar-day"></i>
-            <p>Events</p>
         </a>
         <a id="focus" href="/focus">
             <i class="fa-solid fa-expand"></i>
-            <p>Focus</p>
         </a>
         <a id="archive" href="/archive">
             <i class="fa-solid fa-folder"></i>
-            <p>Archive</p>
         </a>
-    </div>
 </nav>
 
 <style>
@@ -43,49 +37,28 @@
         box-shadow: 0px 0px 8px #888888;
         transition: width 600ms ease;
         overflow: scroll;
-    }
-
-    #sidebarNav {
         display: flex;
         flex-direction: column;
         align-items: center;
-        height: 100%;
-        width: 100%;
+        justify-content: space-around;
     }
 
-    #sidebarNav a {
+    #sidebar a {
         display: flex;
         align-items: center;
+        justify-content: center;
         height: 5rem;
         color: gray;
         text-decoration: none;
         filter: grayscale(100%) opacity(0.7);
         transition: var(--transition-speed);
-    }
-
-    #sidebarNav a {
         width: 100%;
     }
 
-    #sidebarNav:last-child {
+    #sidebar:last-child {
         margin-top: auto;
     }
 
-    
-    #sidebarNav a p {
-        display: none;
-        margin-left: 1rem;
-        filter: opacity(0);
-        transition: var(--transition-speed);
-    }
-
-    #sidebarNav a i {
-        width: 2rem;
-        min-width: 2rem;
-        margin: 0 1.5rem;
-    }
-
-    
     /* Large screens */
     @media only screen and (min-width: 600px) {
         #sidebar {
@@ -93,18 +66,6 @@
             width: 5rem;
             height: 100vh;
         }
-
-        #sidebar:hover {
-            width: 16rem;
-        }
-
-        #sidebar:hover a p {
-            display: inline;
-            filter: opacity(1);
-            transform: translateX(0);
-        }
-        
-
     }
 
     /* Small screens */
@@ -117,11 +78,11 @@
         }
 
 
-        #sidebarNav {
+        #sidebar {
             flex-direction: row;
         }
 
-        #sidebarNav a {
+        #sidebar a {
             justify-content: center;
         }
     }
